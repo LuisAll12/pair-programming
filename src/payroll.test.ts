4b3ac09d486e9fcdea3c82edf9fe7c43fa52547c
Luis Allamand <Luis@g-12.ch>
2025-05-08 16:11:59 +0200

    Tests for 18 yr old to have less money then gross

--- src/payroll.test.ts
+++ src/payroll.test.ts
@@ -8,5 +8,18 @@ describe("calculatePayslip", () => {
             gross: 700,
         };
         expect(() => calculatePayslip(salary)).toThrow("Lernende unter 17 Jahren haben keine Abzüge.");
-    });
+    }),
+    test("Ein 18-Jahriger mit 1200.-", () => {
+        const salary = {
+            born: new Date(Date.parse("2005-01-01 00:00:00+01:00")),
+            payday: new Date(Date.parse("2023-01-01 00:00:00+01:00")),
+            gross: 1200,
+        };
+        const payslip = calculatePayslip(salary);
+        expect(payslip.salary).not.toEqual(salary.gross);
+
+
+
+    }
+)
 })
